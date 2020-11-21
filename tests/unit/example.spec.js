ed0de7b059ac1b74f50992203d7175974a08ed25
Jason Chu <tuanhoangchu@outlook.com>
2020-11-21 12:32:13 +0700

    complete app

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,10 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { shallowMount } from '@vue/test-utils';
+import App from '@/App.vue';
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+describe('App.vue', () => {
+	it('renders a div', () => {
+		const msg = 'new message';
+		const wrapper = shallowMount(App);
+		expect(wrapper.contains('div')).toBe(true);
+	});
 });
